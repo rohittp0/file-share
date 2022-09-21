e8c598f7953cd5876e85e81aec1c54af80e92715
Rohit T P <tprohit9@gmail.com>
2022-09-21 17:10:18 +0530

    powerfully service worker

--- static/index.js
+++ static/index.js
@@ -1,2 +1,11 @@
 if ('serviceWorker' in navigator)
     navigator.serviceWorker.register('/sw.js', {scope: '/'}).then();
+
+const inputs = document.querySelectorAll("body > section input");
+
+function setInputState(state){
+    inputs.forEach(i => i.disabled = state);
+}
+
+window.addEventListener('online', () => setInputState(true));
+window.addEventListener('offline', () => setInputState(false));
--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,66 @@
-self.addEventListener('fetch', event => {
-    if (event.request.method !== 'POST') return;
-    if (event.request.url.indexOf('/app') === -1) return 0;
+const urlsToCache = ["/static/index.js", "/static/files.css", "static/icons/folder.webp", "static/icons/unknown.webp"]
+const CACHE_NAME = "v1";
+
+const putInCache = async (request, response) => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.put(request, response);
+};
+
+const networkFirst = async (request) => {
+    const response = await fetch(request).catch(() => caches.match(request));
+
+    if (response) {
+        putInCache(request, response.clone());
+
+        return response;
+    }
+
+    return new Response("Network error happened", {
+        status: 408,
+        headers: {"Content-Type": "text/plain"},
+    });
+};
+
+const cacheFirst = async (request) => {
+  const cacheResponse = await caches.match(CACHE_NAME);
+
+  if(cacheResponse)
+      return cacheResponse;
+
+  return fetch(request);
+};
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache)));
+
+    return self.skipWaiting()
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(("navigationPreload" in self.registration) && self.registration.navigationPreload.enable());
+
+    // Tell the active service worker to take control of the page immediately.
+    return self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    // We only want to call event.respondWith() if this is a navigation request
+    // for an HTML page.
+    event.respondWith(
+        (async () => {
+            // First, try to use the navigation preload response if it's supported.
+            const preloadResponse = await event.preloadResponse;
+
+            if (preloadResponse) {
+                return preloadResponse;
+            }
+
+            if(["script" , "style", "image"].indexOf(event.request.destination) !== -1)
+                return cacheFirst(event.request)
+
+            // Always try the network first.
+            return await networkFirst(event.request);
+        })()
+    );
 });
